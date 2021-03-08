fa7ab7db2108ec32a722b957addf259f5944b066
Arob7 <66214652+Arob7@users.noreply.github.com>
2021-03-07 22:28:35 -0500

    added JS to file

---
+++ script.js
@@ -0,0 +1,6 @@
+var element = document.getElementById("basic");
+element.innerHTML = "$50.00";
+
+// document.getElementById("basic").innerHTML = "$50.00";
+
+// document.getElementById("professional").innerHTML = "$100.00";document.getElementById("master").innerHTML = "$150.00";
